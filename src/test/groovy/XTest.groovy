160970fdc48c46de717313c769c3e8b1c4fb7be9
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-11-09 16:01:44 +0100

    draft

--- src/test/groovy/XTest.groovy
+++ src/test/groovy/XTest.groovy
@@ -1,5 +1,7 @@
 import spock.lang.Specification
 
+import java.util.concurrent.ForkJoinPool
+
 /**
  * Created by mtumilowicz on 2019-07-30.
  */
@@ -17,5 +19,20 @@ class XTest extends Specification {
     def "test parallelNonIdentityReduce"() {
         expect:
         X.parallelNonIdentityReduce() != 55
+    }
+
+    def "xxx"() {
+        def nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        nums.stream().map({ X.transform(it) }).forEach({})
+        nums.stream().parallel().map({ X.transform(it) }).forEach({}) // work stealing, main thread participates in
+
+        println "DONE " + Thread.currentThread()
+
+        println Runtime.getRuntime().availableProcessors()
+        println ForkJoinPool.commonPool()
+
+        expect:
+        1 == 1
     }
 }
